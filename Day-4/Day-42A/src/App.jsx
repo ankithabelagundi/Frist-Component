cdae3eeac4adba0fb9193e6d2c61bedb00c1f47e
Ankitha R B <ankithabelagundi@gmail.com>
2025-12-19 11:32:02 +0530

    toogle color

---
+++ Day-4/Day-42A/src/App.jsx
@@ -0,0 +1,10 @@
+import ColorToggle from "./ColorToggle"
+function App(){
+  return(
+    <div>
+      <ColorToggle/>
+    </div>
+  )
+}
+
+export default App;
---
+++ Day-4/Day-42A/src/ColorToggle.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function ColorToggle() {
+  const [isRed, setIsRed] = useState(true);
+
+  return (
+    <div>
+      <div
+        style={{
+          color: isRed ? "red" : "blue",
+          fontSize: "20px",
+          marginBottom: "10px"
+        }}
+      >
+        This text changes color
+      </div>
+
+      <button onClick={() => setIsRed(!isRed)}>
+        Toggle Color
+      </button>
+    </div>
+  );
+}
+
+export default ColorToggle;
